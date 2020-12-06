e83ba16ebe856d214ba4824f329eb6201ab4631c
Achintya Ranjan Chaudhary <achintyac77@gmail.com>
2020-12-06 20:00:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+is_instagram = False
+message = 'hello world'
+if is_instagram:
+    for i in range(3000):
+        pyautogui.moveTo(1000, 666, duration=0.1)
+        pyautogui.click()
+        pyautogui.write(message)
+        pyautogui.moveRel(80, 0, duration=0.1)
+        pyautogui.click()
+else:
+    for i in range(3000):
+        pyautogui.moveTo(1281, 698, duration=0.0000000001)
+        pyautogui.click()
+        pyautogui.write(message)
+        pyautogui.moveRel(60, 0, duration=0.0000000001)
+        pyautogui.click()
